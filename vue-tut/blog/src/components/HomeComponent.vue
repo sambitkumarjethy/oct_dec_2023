0d1e2c3c8d23df1755397d7cdd4410d7fbb6a8f0
sambitkumarjethy <sambitkumarjethy@gmail.com>
2023-11-05 11:01:44 +0530

    event calling on button

--- vue-tut/blog/src/components/HomeComponent.vue
+++ vue-tut/blog/src/components/HomeComponent.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Hello from Home component</h1>
     <h2>{{ data }} : {{ msg }}</h2>
+    <button v-on:click="hello('hello called')">Call Hello function</button>
   </div>
 </template>
 <script>
@@ -10,6 +11,11 @@ export default {
   props: {
     data: String,
     msg: String,
+  },
+  methods: {
+    hello(data) {
+      console.log(data);
+    },
   },
 };
 </script>
